0b119654ff18b39a5dde35d93848d81e5417cc15
Daksh Patel <daksh.patel@intellitechwork.com>
2024-01-17 09:57:19 +0530

    add result page

--- src/components/result/result.tsx
+++ src/components/result/result.tsx
@@ -1,3 +1,29 @@
-const Result = () => <div>Result</div>
+import { Box, Heading, List, ListItem } from "@chakra-ui/react"
+
+import { Data } from "pages/home"
+
+interface ResultProps {
+  questionsList: Data | null
+  correctAnswer: string[]
+  wrongAnswer: string[]
+}
+
+const Result = (props: ResultProps) => {
+  const { questionsList, wrongAnswer, correctAnswer } = props
+
+  return (
+    <Box w='full' mt={4}>
+      <Heading as='h2' mb={2}>
+        Result
+      </Heading>
+
+      <List>
+        <ListItem>Total Questions: {questionsList?.results.length}</ListItem>
+        <ListItem>Correct Answer: {correctAnswer.length}</ListItem>
+        <ListItem>Incorrect Answer: {wrongAnswer.length}</ListItem>
+      </List>
+    </Box>
+  )
+}
 
 export default Result
